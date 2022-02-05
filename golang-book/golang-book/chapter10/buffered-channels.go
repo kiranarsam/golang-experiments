232434468f107564bc72ff167b76955ab8b8ddb3
Kiran A <kiranarsam@noreply.com>
2022-02-05 21:54:48 +0530

    removed duplicates

--- golang-book/golang-book/chapter10/buffered-channels.go
+++
@@ -1,7 +0,0 @@
-
-
-// sample 
-c := make(chan int, 1)
-
-// Normally channels are synchronous; both sides of the channel will wait until the other side is ready
-// A buffered channel is asynchronous; sending and receiving a message will not wait unless the channel is already full
